b22acb046d3557cd752073a4435a3cd36edeb35b
Jemner <jemner.vera@agricolaandrea.com>
2026-02-01 23:59:13 -0500

    refactor: Mover ConfigurationPanel a core/ y actualizar imports en AjustesMain

--- frontend/src/components/core/ConfigurationPanel.tsx
+++ frontend/src/components/core/ConfigurationPanel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useTheme } from '../contexts/ThemeContext';
-import { useLanguage } from '../contexts/LanguageContext';
+import { useTheme } from '../../contexts/ThemeContext';
+import { useLanguage } from '../../contexts/LanguageContext';
 
 interface ConfigurationPanelProps {
   className?: string;
