2c4ddbce5c4c49d3d2d610d5e6ab3579fa22fb7c
Jemner <jemner.vera@agricolaandrea.com>
2026-02-02 00:02:14 -0500

    refactor: Crear core/index.ts para centralizar exportaciones

---
+++ frontend/src/components/core/index.ts
@@ -0,0 +1,4 @@
+// Core components - Critical application components
+export { default as LoginForm } from './LoginForm';
+export { default as ConfigurationPanel } from './ConfigurationPanel';
+export { default as WelcomeScreen } from './welcome/WelcomeScreen';
