1ee65ad0fce9dd1a747cb3c21cf78603bb56d5c0
Jemner <jemner.vera@agricolaandrea.com>
2026-02-02 10:57:23 -0500

    chore: Eliminar components/ReglasMain/ (duplicado en features/rules/) - PASO 15.5

--- frontend/src/components/ReglasMain/components/ReglasStatusTab.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ReglasSankeyDiagram } from '../../../features/rules/ReglasSankeyDiagram';
-
-export function ReglasStatusTab() {
-  return (
-    <ReglasSankeyDiagram themeColor="red" />
-  );
-}
