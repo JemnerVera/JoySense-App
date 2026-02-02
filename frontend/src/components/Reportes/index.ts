42029fe37d0f3236885d850249330b786192c72d
Jemner <jemner.vera@agricolaandrea.com>
2026-02-02 10:50:34 -0500

    chore: Eliminar components/Reportes/ (re-export legacy) - PASO 15.1

--- frontend/src/components/Reportes/index.ts
+++
@@ -1,4 +0,0 @@
-// Re-exports legacy de features/reporting para compatibilidad
-export { AlertasMain, AlertasTable } from '../../features/reporting/alertas';
-export { MensajesMain, MensajesTable } from '../../features/reporting/mensajes';
-export { MetricaPorLote, MetricaPorLoteModal, UmbralesPorLote } from '../../features/reporting';
